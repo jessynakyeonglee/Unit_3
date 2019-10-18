53331d621df943bc4f4ddb60832773fce4b0d5a1
leena <leena@ssfs.org>
2019-10-18 11:20:00 -0400

    10/18/19 E block

---
+++ src/NumericComparisons.java
@@ -0,0 +1,10 @@
+public class NumericComparisons {
+    public static void main (String[]args){
+
+        public static boolean isOdd(int num) {
+            return num % 2 != 0:
+        }
+
+
+    }
+}
