1d1b543fb48c1f2d158f0497763c6752f7e6aa43
leena <leena@ssfs.org>
2019-10-21 12:06:13 -0400

    10/18/19 E block

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -1,10 +1,24 @@
-public class NumericComparisons {
-    public static void main (String[]args){
+import java.util.Scanner;
 
-        public static boolean isOdd(int num) {
-            return num % 2 != 0:
-        }
+public class NumericComparisons {
+
+    public static void main(String[]args) {
+
+        System.out.print("Enter the number: ");
+        Scanner NC = new Scanner(System.in);
+        int num = NC.nextInt();
+        System.out.println(isOdd(num));
+
+    }
+        public static boolean isOdd(int num1) { return num1 % 2 == 1 ;}
+        public static boolean isEven(int num1) { return num1 % 2 == 0 ;}
+        public static boolean isZero(int num1) { return num1 == 0 ;}
+        public static boolean isPostive(int num1) { return num1 > 0 ;}
+        public static boolean isNegative(int num1) { return num1 < 0 ;}
+        public static boolean isNonnegative(int num1) { return num1 >= 0 ;}
+        public static boolean isNonpositive(int num1) { return num1 <= 0 ;}
+
 
 
     }
-}
+
