c053afe4f90dbee969b5d2cc0d871bd8ddc40fa1
leena <leena@ssfs.org>
2019-10-22 14:15:11 -0400

    10/22/19 E block with part II

---
+++ src/AddToPositiveOrNegativeSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AddToPositiveOrNegativeSum {
+    public static void main(String[]args) {
+
+        Scanner J = new Scanner(System.in);
+        //declare and instantiate a Scanner variable and object
+        //declare and initialize a positive and negative sum
+        for (int i = 1; i <= 10; i++)        {
+            /* in this block, allow the user to input a number and add the number to the appropriate sum */
+        }
+        //output the sums    }
+    }
+
+
+
+
+
+}
