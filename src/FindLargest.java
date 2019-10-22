75e7585217c180891eda318c07ea1cafb86998cc
leena <leena@ssfs.org>
2019-10-22 13:59:22 -0400

    10/22/19 E block with part II

---
+++ src/FindLargest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FindLargest {
+    public static void main (String[]args){
+
+        System.out.println("Enter the first number");
+        System.out.println("Enter the second number");
+        Scanner FL = new Scanner(System.in);
+        int num1 = FL.nextInt();
+        int num2 = FL.nextInt();
+
+        if(num1>num2);{
+            System.out.println(num1+" is larger than "+num2);
+
+        }
+
+        if(num1==num2);{
+            System.out.println(num1+" is equal to "+num2);
+        }
+
+
+
+
+    }
+
+}
--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -9,15 +9,15 @@ public class NumericComparisons {
         Scanner NC = new Scanner(System.in);
         int num = NC.nextInt();
         int num2 = NC.nextInt();
-        System.out.println(isOdd(num));
-        System.out.println(isEven(num));
-        System.out.println(isZero(num));
-        System.out.println(isPostive(num));
-        System.out.println(isNegative(num));
-        System.out.println(isNonnegative(num));
-        System.out.println(isNonpositive(num));
-        System.out.println(isFactor(num,num2));
-        System.out.println(isMultiple(num,num2));
+        System.out.println(num+" is odd: "+isOdd(num));
+        System.out.println(num+" is even: "+isEven(num));
+        System.out.println(num+" is zero: "+isZero(num));
+        System.out.println(num+" is positive: "+isPostive(num));
+        System.out.println(num+" is negative: "+isNegative(num));
+        System.out.println(num+" is nonnegative: "+isNonnegative(num));
+        System.out.println(num+" is nonpositive: "+isNonpositive(num));
+        System.out.println(num+" is a factor of "+num2+": "+isFactor(num,num2));
+        System.out.println(num2+" is a factor of "+num+": "+isMultiple(num,num2));
 
     }
         public static boolean isOdd(int num1) { return num1 % 2 == 1 ;}
