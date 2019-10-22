3238e8806acfc9d20e2947585907bfbcd0bec1ad
leena <leena@ssfs.org>
2019-10-22 13:41:29 -0400

    10/22/19 E block with part II

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -4,9 +4,11 @@ public class NumericComparisons {
 
     public static void main(String[]args) {
 
-        System.out.print("Enter the number: ");
+        System.out.println("Enter the number: ");
+        System.out.println("Enter another number: ");
         Scanner NC = new Scanner(System.in);
         int num = NC.nextInt();
+        int num2 = NC.nextInt();
         System.out.println(isOdd(num));
         System.out.println(isEven(num));
         System.out.println(isZero(num));
@@ -14,6 +16,8 @@ public class NumericComparisons {
         System.out.println(isNegative(num));
         System.out.println(isNonnegative(num));
         System.out.println(isNonpositive(num));
+        System.out.println(isFactor(num,num2));
+        System.out.println(isMultiple(num,num2));
 
     }
         public static boolean isOdd(int num1) { return num1 % 2 == 1 ;}
@@ -23,6 +27,9 @@ public class NumericComparisons {
         public static boolean isNegative(int num1) { return num1 < 0 ;}
         public static boolean isNonnegative(int num1) { return num1 >= 0 ;}
         public static boolean isNonpositive(int num1) { return num1 <= 0 ;}
+        public static boolean isFactor(int num1,int num2) { return num2%num1 == 0 ;}
+        public static boolean isMultiple(int num1,int num2) { return num1%num2 == 0 ;}
+
 
 
 
