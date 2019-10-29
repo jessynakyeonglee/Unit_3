a10d4665290548dcc0bf36198da76d5add76c6af
leena <leena@ssfs.org>
2019-10-29 11:20:35 -0400

    10/29/19 E block

---
+++ src/TrickyDice.java
@@ -0,0 +1,13 @@
+public class TrickyDice {
+    public static void main(String[]args){
+
+
+    }
+    public static int regularRoll(){
+        int num = (int)(Math.random()*6+1);
+        return num;
+    }
+    public static int trickyRoll(){
+
+    }
+}
