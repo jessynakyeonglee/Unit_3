25c13b97c93c81df37fa612fbe007716b40b630a
leena <leena@ssfs.org>
2019-11-01 15:08:47 -0400

    11/1/19 E block

---
+++ src/CookieOrders.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CookieOrders {
+    public static void main(String[]args) {
+
+        Scanner wetcookie = new Scanner(System.in);
+        System.out.println("Enter the number of boxes you wish to purchase: ");
+        int box = wetcookie.nextInt();
+
+    }
+    public static String TotalCost(int numboxes){
+        if (numboxes>0) {
+            double cost = 6.95;
+            double total = cost*box
+        }
+            return "6.95";
+        if (numboxes>4)
+            return
+        }
+
+}
